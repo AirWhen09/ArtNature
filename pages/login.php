5ae9d794479b85f71f9ff896b1a48fa96e298988
Erwin <ibarra.erwin09@gmail.com>
2022-08-07 06:22:43 +0800

    add login page

--- pages/login.php
+++ pages/login.php
@@ -1,35 +1,58 @@
 
 <div class="container login-section">
-    <div class="login-container col-lg-10 mx-auto  shadow-lg p-4">
+    <div class="card login-container shadow-lg p-5">
         <div class="card-title">
             <h2>ArtNature: Libmanan Production Management</h2>
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col-lg-4">
-                    <div>
-                        <img 
+                <div class="col-lg-5">
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                    <h4>Welcome</h4>
+                    <img 
                         src="http://www.rpghairwigs.com/skin/frontend/rwd/default/images/logo.png" 
                         alt="logo"
                         class="img-fluid"
                         >
                     </div>
                 </div>
-                <div class="col-lg-8">
-                    <form action="" class="pt-2 needs-validation" novalidate>
-                        <div class="mb-3">
-                          <label for="username" class="form-label">Username</label>
-                          <input 
-                          type="text" 
-                          class="form-control login-input" 
-                          name="username" 
-                          id="username" 
-                          aria-describedby="usernameId" 
-                          placeholder="Type here..."
-                          required>
-                          <small id="usernameId" class="form-text text-muted"></small>
-                        </div>
-                    </form>
+                <div class="col-lg-7 ">
+                    <div class="container p-4">
+                        <form action="" class="pt-2 needs-validation p3" novalidate>
+                            <div class="mb-3">
+                            <label for="username" class="form-label login-label">Username</label>
+                            <input 
+                            type="text" 
+                            class="form-control login-input" 
+                            name="username" 
+                            id="username" 
+                            aria-describedby="usernameId" 
+                            placeholder="Type here..."
+                            required>
+                            <small id="usernameId" class="form-text text-muted"></small>
+                            </div>
+                            <div class="mb-3">
+                            <label for="password" class="form-label login-label">Password</label>
+                            <input 
+                            type="password" 
+                            class="form-control login-input" 
+                            name="password" 
+                            id="password" 
+                            aria-describedby="passwordId" 
+                            placeholder="Type here..."
+                            required>
+                            <small id="passwordId" class="form-text text-muted"></small>
+                            </div>
+                            <div class="d-grid gap-2">
+                            <button 
+                            type="submit" 
+                            name="login" 
+                            id="login" 
+                            class="btn login-input primary-btn"
+                            >Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
