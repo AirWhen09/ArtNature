13a02b382a9aa65d8a874f96ac7d5489989ce12d
Erwin <ibarra.erwin09@gmail.com>
2022-08-07 09:09:40 +0800

    add some changes

--- pages/login.php
+++ pages/login.php
@@ -14,42 +14,47 @@
                         alt="logo"
                         class="img-fluid"
                         >
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio excepturi dolore ad velit ipsum asperiores aspernatur sequi dolores placeat officiis, dolor quo nisi autem fugit quis voluptatum nemo accusantium dignissimos?</p>
                     </div>
                 </div>
                 <div class="col-lg-7 ">
                     <div class="container p-4">
                         <form action="" class="pt-2 needs-validation p3" novalidate>
                             <div class="mb-3">
-                            <label for="username" class="form-label login-label">Username</label>
-                            <input 
-                            type="text" 
-                            class="form-control login-input" 
-                            name="username" 
-                            id="username" 
-                            aria-describedby="usernameId" 
-                            placeholder="Type here..."
-                            required>
-                            <small id="usernameId" class="form-text text-muted"></small>
+                                <label for="username" class="form-label login-label">Username</label>
+                                <input 
+                                type="text" 
+                                class="form-control login-input" 
+                                name="username" 
+                                id="username" 
+                                aria-describedby="usernameId" 
+                                placeholder="Type here..."
+                                required>
+                                <small id="usernameId" class="form-text text-muted"></small>
                             </div>
                             <div class="mb-3">
-                            <label for="password" class="form-label login-label">Password</label>
-                            <input 
-                            type="password" 
-                            class="form-control login-input" 
-                            name="password" 
-                            id="password" 
-                            aria-describedby="passwordId" 
-                            placeholder="Type here..."
-                            required>
-                            <small id="passwordId" class="form-text text-muted"></small>
+                                <label for="password" class="form-label login-label">Password</label>
+                                <input 
+                                type="password" 
+                                class="form-control login-input" 
+                                name="password" 
+                                id="password" 
+                                aria-describedby="passwordId" 
+                                placeholder="Type here..."
+                                required>
+                                <small id="passwordId" class="form-text text-muted"></small>
                             </div>
                             <div class="d-grid gap-2">
-                            <button 
-                            type="submit" 
-                            name="login" 
-                            id="login" 
-                            class="btn login-input primary-btn"
-                            >Login</button>
+                                <button 
+                                type="submit" 
+                                name="login" 
+                                id="login" 
+                                class="btn login-input primary-btn"
+                                >Login</button>
+                            </div>
+                            <div class="d-flex flex-column justify-content-center item-content-center mt-2">
+                                <p class="text-center">or</p>
+                                <p class="text-center">Don't have an account yet? Sign up instead</p>
                             </div>
                         </form>
                     </div>
